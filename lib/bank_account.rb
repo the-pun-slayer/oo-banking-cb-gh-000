31b53e7c2cabd59d057d109e0ebde85093cf2d73
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-08 19:43:35 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,11 +2,19 @@ class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
   
+  @@all=[]
+  
   def initialize(name)
     @name=name
     @balance= 1000
     @status= "open"
+    @all<< name
   end
+  
+  def deposit(amt)
+    @balance+=amt
+  end
+  
   
   
 
