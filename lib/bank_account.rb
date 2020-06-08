f7164216c8e529c1fe4ec916e1fcdd65c2752fc2
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-08 20:02:11 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
-
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  @@all=[]
+  
+  def initialize(name)
+    @name=name
+    @balance= 1000
+    @status= "open"
+    @@all<< name
+  end
+  
+  def deposit(amt)
+    @balance+=amt
+  end
+  
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+  
+  def valid?
+    self.status=="open"&& self.balance>0
+  end
+      
+  def close_account 
+    self.status="closed"
+  end
+  
+  
 end
